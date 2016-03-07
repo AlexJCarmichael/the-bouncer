448131be69bbcecc46d0c3a6baae79129274ed34
Dane Carmichael <carmichaeldane@gmail.com>
2016-03-07 16:20:06 -0600

    "Adventure mode finished, needed help from Vivek to solve a looping problem."

--- bouncer.rb
+++ bouncer.rb
@@ -1,21 +1,35 @@
 #The bouncer will verify the users number input and let the user know what age restrictions they have.
+def start_loop (input)
+  if  input == "quit"
+    puts "Good-bye"
+    exit
+  else
+    check_age(input.to_i)
+  end
+end
+
 def check_age(age)
-  case
-  when age < 1
-    puts "The age given really should be greater than 0. Or an integer."
-  when age < 18
-    puts "There are no vices in which the government has put an age limit on that you are allowed to partake in."
-  when age < 21
-    puts "You are now allowed to take part in the vices of smoking and voting."
-  when age < 25
-    puts "In addition the previous vices of smoking and voting, you may now include the legal consumption of alcohol."
-  when age >= 25
-    puts "In addition to the previous vices of smoking, voting, and drinking, you now participate in the worst, car rental."
+#age.to_i
+#puts "The class is #{age.class}"
+  while true
+    case
+    when age < 1
+      puts "The age given really should be greater than 0. Or an integer."
+    when age < 18
+      puts "There are no vices in which the government has put an age limit on that you are allowed to partake in."
+    when age < 21
+      puts "You are now allowed to take part in the vices of smoking and voting."
+    when age < 25
+      puts "In addition the previous vices of smoking and voting, you may now include the legal consumption of alcohol."
+    when age >= 25
+      puts "In addition to the previous vices of smoking, voting, and drinking, you now participate in the worst, car rental."
+    end
+    print "> "
+    start_loop (input = gets.chomp)
   end
 end
 
 puts "The Bouncer will tell you what a person of a certain age is allowed to do. Please give The Bouncer your age."
 print "> "
-age = gets.chomp.to_i
 
-check_age(age)
+start_loop (input = gets.chomp)
